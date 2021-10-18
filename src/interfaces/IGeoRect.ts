4cdadc39d80cb445145130c4bb8ad6b9fc17ffb9
Ivan Reshetnikov <resivalex@gmail.com>
2021-10-19 00:20:59 +0300

    Single scale heatmap with hardcoded data

---
+++ src/interfaces/IGeoRect.ts
@@ -0,0 +1,6 @@
+export default interface IGeoRect {
+	minLat: number;
+	minLng: number;
+	maxLat: number;
+	maxLng: number;
+}
---
+++ src/services/geoMatrix.ts
@@ -0,0 +1,100 @@
+import { IGeoRect } from "../interfaces";
+
+export function getSubMatrixForRectSelection(
+	selection: IGeoRect,
+	matrix: number[][],
+	matrixGeoRect: IGeoRect
+): { geoRect: IGeoRect; matrix: number[][] } {
+	if (
+		selection.minLat > selection.maxLat ||
+		selection.minLng > selection.maxLng
+	) {
+		throw new Error(
+			"Selection must be sorted minLat < maxLat, minLng < maxLng"
+		);
+	}
+	const vertN = matrix.length;
+	const horzN = matrix[0].length;
+	const vertStep = (matrixGeoRect.maxLat - matrixGeoRect.minLat) / vertN;
+	const horzStep = (matrixGeoRect.maxLng - matrixGeoRect.minLng) / horzN;
+
+	const vertMinIndex = Math.min(
+		Math.max(
+			getCoveredIndex(
+				selection.minLat,
+				matrixGeoRect.minLat,
+				matrixGeoRect.maxLat,
+				vertN
+			),
+			0
+		),
+		vertN - 1
+	);
+	const horzMinIndex = Math.min(
+		Math.max(
+			getCoveredIndex(
+				selection.minLng,
+				matrixGeoRect.minLng,
+				matrixGeoRect.maxLng,
+				horzN
+			),
+			0
+		),
+		horzN - 1
+	);
+	const vertMaxIndex = Math.min(
+		Math.max(
+			getCoveredIndex(
+				selection.maxLat,
+				matrixGeoRect.minLat,
+				matrixGeoRect.maxLat,
+				vertN
+			),
+			0
+		),
+		vertN - 1
+	);
+	const horzMaxIndex = Math.min(
+		Math.max(
+			getCoveredIndex(
+				selection.maxLng,
+				matrixGeoRect.minLng,
+				matrixGeoRect.maxLng,
+				horzN
+			),
+			0
+		),
+		horzN - 1
+	);
+	const resultBorders: IGeoRect = {
+		minLat: matrixGeoRect.minLat + vertMinIndex * vertStep,
+		minLng: matrixGeoRect.minLng + horzMinIndex * horzStep,
+		maxLat: matrixGeoRect.minLat + (vertMaxIndex + 1) * vertStep,
+		maxLng: matrixGeoRect.minLng + (horzMaxIndex + 1) * horzStep
+	};
+	const resultMatrix = [];
+	for (let i = vertMinIndex; i <= vertMaxIndex; i++) {
+		const row = [];
+		for (let j = horzMinIndex; j < horzMaxIndex; j++) {
+			row.push(matrix[i][j]);
+		}
+		resultMatrix.push(row);
+	}
+	return {
+		geoRect: resultBorders,
+		matrix: resultMatrix
+	};
+}
+
+function getCoveredIndex(
+	x: number,
+	left: number,
+	right: number,
+	steps: number
+): number {
+	const length = right - left;
+	const shift = x - left;
+	const relativePosition = shift / length;
+
+	return Math.floor(relativePosition * steps);
+}
