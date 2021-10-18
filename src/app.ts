4debce0ef606af116904d5c0c4fcce2891ef9d48
Abdelsalam Shadi <floydanthebeast@gmail.com>
2021-10-18 18:47:42 +0300

    feat(docs): Add OpenAPI documentation - Serving documentation preview on '/api-docs' route

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,24 @@
 import express from "express";
+import path from "path";
 import {
 	RoutingControllersOptions,
 	useExpressServer
 } from "routing-controllers";
+import swaggerUI from "swagger-ui-express";
+import YAML from "yamljs";
+
+const swaggerDoc = YAML.load(path.join(__dirname, "openapi.yml"));
 
 class App {
 	private server: express.Application;
 
 	constructor(options: RoutingControllersOptions) {
 		this.server = express();
+		this.server.use(
+			"/api-docs",
+			swaggerUI.serve,
+			swaggerUI.setup(swaggerDoc)
+		);
 
 		useExpressServer(this.server, { ...options });
 	}
